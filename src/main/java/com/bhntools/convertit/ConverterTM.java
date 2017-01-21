f6921a10fa55745f28c56540c72c1a8d0344e66d
NajlaBH-JX <bhndevtools@gmail.com>
2017-01-21 21:28:35 +0100

    Refactor Files Names : Main and FXML controller .

--- src/main/java/com/bhntools/convertit/ConverterTM.java
+++ src/main/java/com/bhntools/convertit/ConverterTM.java
@@ -6,9 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import static javafx.application.Application.launch;
+import static javafx.application.Application.launch;
+import static javafx.application.Application.launch;
 
 
-public class MainApp extends Application {
+public class ConverterTM extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
